4dd9c7c45dd858dd92f0f21b15ad325cd25e5db4
NguyenXu <xu2603@gmail.com>
2019-09-04 20:15:12 +0700

    create login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './pages/login/login';
 
 function Nhi() {
   return (
     <div className="Nhi">
+      <LoginPage></LoginPage>
     </div>
   );
 }
