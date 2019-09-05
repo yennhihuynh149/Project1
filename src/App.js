98f17a59b1e9bd99f37f0586eee738f80db5e727
Nhi <yennhihuynh149@gmail.com>
2019-09-05 22:40:46 +0700

    create 2 page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './pages/login/login';
+import Profile from './pages/profile/profile';
+import DashBoard from './pages/dashboard/db';
 
 function Nhi() {
   return (
     <div className="Nhi">
       <LoginPage></LoginPage>
+      <Profile></Profile>
+      <DashBoard></DashBoard>
     </div>
   );
 }
---
+++ src/pages/dashboard/db.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class DashBoard extends React.Component{
+    render(){
+        return(
+            <div>DashBoard Page</div>
+        )
+    }
+}
+
+export default DashBoard
