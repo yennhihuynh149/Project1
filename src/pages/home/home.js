4466765cc6a11d0bacd730b74e5be170b3fccfb2
yennhihuynh149 <yennhihuynh149@gmail.com>
2019-09-20 21:24:27 +0700

    check login

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,22 +4,7 @@ class HomePage extends React.Component{
     render(){
         return(
             <React.Fragment>
-                <div className="home-page">
-                    <div className="what_we_do_contact">
-                        <div className="what-we-do">
-                        Affronting imprudence do he he everything. Sex lasted dinner wanted indeed wished out law. Far advanced settling say finished raillery. Offered chiefly farther of my no colonel shyness. Such on help ye some door if in. Laughter proposal laughing any son law consider. Needed except up piqued an. 
-                        </div>
-                        <div className="contact">
-                            Contact
-                        </div>
-                    </div>
-                    <div className="cart-item">
-                        <div className="item">Affronting imprudence do he he everything. Sex lasted dinner wanted indeed wished out law. Far advanced settling say finished raillery. Offered chiefly farther of my no colonel shyness. Such on help ye some door if in. Laughter proposal laughing any son law consider. Needed except up piqued an.</div>
-                        <div className="item">Affronting imprudence do he he everything. Sex lasted dinner wanted indeed wished out law. Far advanced settling say finished raillery. Offered chiefly farther of my no colonel shyness. Such on help ye some door if in. Laughter proposal laughing any son law consider. Needed except up piqued an.</div>
-                        <div className="item">Affronting imprudence do he he everything. Sex lasted dinner wanted indeed wished out law. Far advanced settling say finished raillery. Offered chiefly farther of my no colonel shyness. Such on help ye some door if in. Laughter proposal laughing any son law consider. Needed except up piqued an.</div>
-                    </div>
-                    
-                </div>
+                <div>Home Page</div>
             </React.Fragment>
         )
     }
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,20 +1,47 @@
 import React from 'react';
 import './login.scss';
 import {Form, Button} from 'react-bootstrap';
+import Cookies from 'universal-cookie';
+import { Redirect } from 'react-router-dom';
 
 class LoginPage extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div className="main">
-                    <div className="login">
-                        <div className="circle">
-                        </div>
-                    </div>
-                </div>
-            </React.Fragment>
-        )
+
+  constructor(){
+    super()
+    this.state = {
+      logined: false
     }
+  }
+
+  onClick = (event) => {
+    console.log(event)
+    console.log(event.currentTarget)
+    let cookie = new Cookies()
+    cookie.set('isLogin', 'true')
+    this.setState({
+      logined: cookie.get('isLogin') === 'true'
+    })
+  }
+
+  renderDirectTo() {
+    if (this.state.logined) {
+      return <Redirect to = '/home'></Redirect>
+    }
+  }
+  render() {
+      return (
+          <React.Fragment>
+            {this.renderDirectTo()}
+              <div className="main">
+                  <div className="login">
+                      <div className="circle">
+                      </div>
+                  </div>
+                  <Button onClick={(e) => this.onClick(e)}>Login</Button>
+              </div>
+          </React.Fragment>
+      )
+  }
 }
 
 export default LoginPage
